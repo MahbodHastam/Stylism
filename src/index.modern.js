86ae6b8466b0ee45ab28ccf11f0307f81c8499f6
Mahbod <mahbod4782@gmail.com>
2021-03-30 01:50:14 +0430

    add Tooltip component

--- src/index.modern.js
+++ src/index.modern.js
@@ -1,145 +1,97 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const logError = message => {
-  throw new Error(`Stylism: ${message}`);
+function _taggedTemplateLiteralLoose(strings, raw) {
+  if (!raw) {
+    raw = strings.slice(0);
+  }
+
+  strings.raw = raw;
+  return strings;
+}
+
+var logError = function logError(message) {
+  throw new Error("Stylism: " + message);
 };
 
-let _ = t => t,
-    _t;
+var _templateObject;
 
-const GradientText = props => {
-  const {
-    children,
-    degree = 45,
-    startColor,
-    endColor,
-    hasHover,
-    transitionTime,
-    fontSize,
-    id
-  } = props;
-  if (!startColor || !endColor) logError(`'startColor' or 'endColor' cannot be empty!`);
-  const hoverStyles = `
-    background-position: left;
-    background-size: 200%;
-    :hover {
-      background-position: right;
-    }
-  `;
+var GradientText = function GradientText(props) {
+  var children = props.children,
+      _props$degree = props.degree,
+      degree = _props$degree === void 0 ? 45 : _props$degree,
+      startColor = props.startColor,
+      endColor = props.endColor,
+      hasHover = props.hasHover,
+      transitionTime = props.transitionTime,
+      fontSize = props.fontSize,
+      id = props.id;
+  if (!startColor || !endColor) logError("'startColor' or 'endColor' cannot be empty!");
+  var hoverStyles = "\n    background-position: left;\n    background-size: 200%;\n    :hover {\n      background-position: right;\n    }\n  ";
 
-  const transitionStyles = time => `
-    transition: all ${time}ms ease;
-  `;
+  var transitionStyles = function transitionStyles(time) {
+    return "\n    transition: all " + time + "ms ease;\n  ";
+  };
 
-  const MyStyle = styled.span(_t || (_t = _`
-    color: transparent;
-    font-size: ${0};
-    background-image: linear-gradient(${0}deg, ${0}, ${0});
-    background-clip: text;
-    background-position: center;
-    background-size: 100%;
-    ${0}
-    ${0}
-  `), fontSize || '', degree, startColor, endColor, hasHover ? hoverStyles : '', transitionTime ? transitionStyles(transitionTime) : '');
+  var MyStyle = styled.span(_templateObject || (_templateObject = _taggedTemplateLiteralLoose(["\n    color: transparent;\n    font-size: ", ";\n    background-image: linear-gradient(", "deg, ", ", ", ");\n    background-clip: text;\n    background-position: center;\n    background-size: 100%;\n    ", "\n    ", "\n  "])), fontSize || '', degree, startColor, endColor, hasHover ? hoverStyles : '', transitionTime ? transitionStyles(transitionTime) : '');
   return /*#__PURE__*/React.createElement(MyStyle, {
     id: id || ''
   }, children);
 };
 
-let _$1 = t => t,
-    _t$1;
+var _templateObject$1;
 
-const ButtonGradient = props => {
-  const {
-    children,
-    degree = 45,
-    textColor = '#fff',
-    startColor,
-    endColor,
-    hasHover,
-    transitionTime,
-    fontSize,
-    style,
-    id
-  } = props;
-  if (!startColor || !endColor) logError(`'startColor' or 'endColor' cannot be empty!`);
-  const hoverStyles = `
-    background-position: left;
-    background-size: 200%;
-    :hover, :focus {
-      background-position: right;
-    }
-  `;
+var ButtonGradient = function ButtonGradient(props) {
+  var children = props.children,
+      _props$degree = props.degree,
+      degree = _props$degree === void 0 ? 45 : _props$degree,
+      _props$textColor = props.textColor,
+      textColor = _props$textColor === void 0 ? '#fff' : _props$textColor,
+      startColor = props.startColor,
+      endColor = props.endColor,
+      hasHover = props.hasHover,
+      transitionTime = props.transitionTime,
+      fontSize = props.fontSize,
+      style = props.style,
+      id = props.id;
+  if (!startColor || !endColor) logError("'startColor' or 'endColor' cannot be empty!");
+  var hoverStyles = "\n    background-position: left;\n    background-size: 200%;\n    :hover, :focus {\n      background-position: right;\n    }\n  ";
 
-  const transitionStyles = time => `
-    transition: all ${time}ms ease;
-  `;
+  var transitionStyles = function transitionStyles(time) {
+    return "\n    transition: all " + time + "ms ease;\n  ";
+  };
 
-  const MyStyle = styled.button(_t$1 || (_t$1 = _$1`
-    color: ${0};
-    outline: 0 !important;
-    border: 0 !important;
-    cursor: pointer;
-    font-size: ${0};
-    background-image: linear-gradient(${0}deg, ${0}, ${0});
-    background-position: center;
-    background-size: 100%;
-    ${0}
-    ${0}
-    ${0}
-  `), textColor, fontSize || '1em', degree, startColor, endColor, hasHover ? hoverStyles : '', transitionTime ? transitionStyles(transitionTime) : '', style || '');
+  var MyStyle = styled.button(_templateObject$1 || (_templateObject$1 = _taggedTemplateLiteralLoose(["\n    color: ", ";\n    outline: 0 !important;\n    border: 0 !important;\n    cursor: pointer;\n    font-size: ", ";\n    background-image: linear-gradient(", "deg, ", ", ", ");\n    background-position: center;\n    background-size: 100%;\n    ", "\n    ", "\n    ", "\n  "])), textColor, fontSize || '1em', degree, startColor, endColor, hasHover ? hoverStyles : '', transitionTime ? transitionStyles(transitionTime) : '', style || '');
   return /*#__PURE__*/React.createElement(MyStyle, {
     id: id || ''
   }, children);
 };
 
-let _$2 = t => t,
-    _t$2;
+var _templateObject$2;
 
-const Tooltip = props => {
-  const {
-    text,
-    position = 'top',
-    textColor = '#fff',
-    backColor = 'rgba(0, 0, 0, .5)',
-    parentId = null,
-    transitionTime = 50,
-    fontSize = '0.75em'
-  } = props;
-  if (!text) logError(`'text' cannot be empty!`);
+var Tooltip = function Tooltip(props) {
+  var text = props.text,
+      _props$position = props.position,
+      position = _props$position === void 0 ? 'top' : _props$position,
+      _props$textColor = props.textColor,
+      textColor = _props$textColor === void 0 ? '#fff' : _props$textColor,
+      _props$backColor = props.backColor,
+      backColor = _props$backColor === void 0 ? 'rgba(0, 0, 0, .5)' : _props$backColor,
+      _props$parentId = props.parentId,
+      parentId = _props$parentId === void 0 ? null : _props$parentId,
+      _props$transitionTime = props.transitionTime,
+      transitionTime = _props$transitionTime === void 0 ? 50 : _props$transitionTime,
+      _props$fontSize = props.fontSize,
+      fontSize = _props$fontSize === void 0 ? '0.75em' : _props$fontSize;
+  if (!text) logError("'text' cannot be empty!");
 
-  const transitionStyles = time => `
-    transition: all ${time}ms ease;
-  `;
+  var transitionStyles = function transitionStyles(time) {
+    return "\n    transition: all " + time + "ms ease;\n  ";
+  };
 
-  const Styles = styled.span(_t$2 || (_t$2 = _$2`
-    color: ${0};
-    background-color: ${0};
-    padding: 4px 8px;
-    border-radius: 0.3rem;
-    font-size: ${0};
-    position: absolute;
-    ${0}: -20px;
-    opacity: 0;
-    visibility: hidden;
-    width: 85%;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: -1;
-    ${0}
-  `), textColor, backColor, fontSize, position, transitionTime ? transitionStyles(transitionTime) : '');
-  const styleElement = document.createElement('style');
-  styleElement.innerHTML = `
-    #${parentId} {
-      position: relative;
-    }
-    #${parentId}:hover > span {
-      opacity: 1;
-      visibility: visible;
-      ${position}: -80%;
-    }
-  `;
+  var Styles = styled.span(_templateObject$2 || (_templateObject$2 = _taggedTemplateLiteralLoose(["\n    color: ", ";\n    background-color: ", ";\n    padding: 4px 8px;\n    border-radius: 0.3rem;\n    font-size: ", ";\n    position: absolute;\n    ", ": -20px;\n    opacity: 0;\n    visibility: hidden;\n    width: 85%;\n    left: 50%;\n    transform: translateX(-50%);\n    z-index: -1;\n    ", "\n  "])), textColor, backColor, fontSize, position, transitionTime ? transitionStyles(transitionTime) : '');
+  var styleElement = document.createElement('style');
+  styleElement.innerHTML = "\n    #" + parentId + " {\n      position: relative;\n    }\n    #" + parentId + ":hover > span {\n      opacity: 1;\n      visibility: visible;\n      " + position + ": -80%;\n    }\n  ";
   document.querySelector('head').append(styleElement);
   console.log(text, position, textColor, backColor);
   return /*#__PURE__*/React.createElement(Styles, null, text);
